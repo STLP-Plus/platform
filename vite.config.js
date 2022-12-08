a7117840bcbdb78362d0c99a0e530225ffb5cfaa
Kevin Muscara <muscarak1@nku.edu>
2022-12-08 17:09:29 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
-    port: 3000,
+    port: 80,
     host: '0.0.0.0',
   },
   build: {
